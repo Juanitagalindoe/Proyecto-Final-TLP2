258f2ee2b5b2580d46fa47ee56c3dca2a4287446
juancho-123 <148466225+juancho-123@users.noreply.github.com>
2025-12-07 10:46:29 -0500

    Delete backend directory

--- backend/src/main/java/com/proyecto_final/backend/Exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.proyecto_final.backend.Exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String msg) {
-        super(msg);
-    }
-}
